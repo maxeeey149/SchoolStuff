213a9ec2100b831c8c4fa108af6ca61b2c42f352
maxeeey149 <iftenie.maxi@gmx.de>
2025-06-19 22:28:49 +0200

    add nginx reverse proxy

--- Frontend/app.js
+++ Frontend/app.js
@@ -7,7 +7,8 @@ input.addEventListener("keypress", function(event) {
 
 //var localAdress = "localhost:9000";
 //var localAdress = "http://192.168.0.42:9000";
-var localAdress = "todoliste.maxeeey.com:9000";
+//var localAdress = "todoliste.maxeeey.com:9000";
+var localAdress = "/api";
 
 function displayTODOList(todoList) {
     const todoListContainer = document.querySelector(".TODOListPlaceholder");
